b912fd2e20799c387ecb6b9a7644614f5b2018bc
Zoe Ma <rm4069@columbia.edu>
2023-09-29 19:02:40 +0000

    part 1 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,15 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+
+    @all_ratings = Movie.all_ratings
+    # @movies = Movie.all
+    if !params[:ratings].nil?
+      @ratings_to_show = params[:ratings].map{|rating| rating.upcase}
+    else
+      @ratings_to_show = @all_ratings
+    
+    
   end
 
   def new
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,15 @@
 class Movie < ActiveRecord::Base
+
+  def self.all_ratings
+    ['G','PG','PG-13','R']
+  end
+
+
+  def self.with_ratings(ratings_list)
+  # if ratings_list is an array such as ['G', 'PG', 'R'], retrieve all
+  #  movies with those ratings
+  # if ratings_list is nil, retrieve ALL movies
+    where(ratings: ratings_list)
+  end
+
 end
